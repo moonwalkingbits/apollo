1c7105abb43aa1e7ff4de8ed3ba51e519c73d5a6
Martin Pettersson <martin_pettersson@outlook.com>
2020-12-04 08:15:10 +0100

    Add typescript validation for components

--- index.test-d.ts
+++ index.test-d.ts
@@ -9,6 +9,12 @@ import { expectAssignable } from "tsd";
 
 import { Container, ContainerInterface } from "./container";
 import { Configuration, ConfigurationInterface } from "./configuration";
+import { LogHandlerInterface, Logger } from "./log";
+import { UrlFactoryInterface, UrlFactory } from "./http";
+import { ServerBuilderInterface, ServerBuilder } from "./server";
 
 expectAssignable<ConfigurationInterface>(new Configuration());
 expectAssignable<ContainerInterface>(new Container());
+expectAssignable<LogHandlerInterface>(new Logger([]));
+expectAssignable<UrlFactoryInterface>(new UrlFactory());
+expectAssignable<ServerBuilderInterface>(new ServerBuilder());
