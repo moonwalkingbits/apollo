ebaf10059cda0f30cf221c656db8102b2ff21e5d
Martin Pettersson <martin_pettersson@outlook.com>
2020-12-04 10:27:25 +0100

    Add type definitions for startup class

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,32 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare type ConfigurationInterface = import("@moonwalkingbits/apollo-configuration").ConfigurationInterface;
+declare type ContainerInterface = import("@moonwalkingbits/apollo-container").ContainerInterface;
+declare type StartupInterface = import("@moonwalkingbits/apollo-server").StartupInterface;
+
+/**
+ * A minimal default startup routine.
+ */
+declare class Startup implements StartupInterface {
+    /**
+     * Create a new startup instance.
+     *
+     * @param configuration Application configuration instance.
+     */
+    public constructor(configuration: ConfigurationInterface);
+
+    /**
+     * Load and run all configured service providers.
+     *
+     * All service providers are constructed using the application container to
+     * enable dependency injection.
+     *
+     * @param container Application container instance.
+     * @return A promise resolving when the method is done.
+     */
+    public configure(configuration: ContainerInterface): Promise<void>;
+}
+
+export { Startup };
--- index.test-d.ts
+++ index.test-d.ts
@@ -11,10 +11,15 @@ import { Container, ContainerInterface } from "./container";
 import { Configuration, ConfigurationInterface } from "./configuration";
 import { LogHandlerInterface, Logger } from "./log";
 import { UrlFactoryInterface, UrlFactory } from "./http";
-import { ServerBuilderInterface, ServerBuilder } from "./server";
+import { ServerBuilderInterface, ServerBuilder, StartupInterface } from "./server";
+import { Startup } from ".";
 
 expectAssignable<ConfigurationInterface>(new Configuration());
 expectAssignable<ContainerInterface>(new Container());
 expectAssignable<LogHandlerInterface>(new Logger([]));
 expectAssignable<UrlFactoryInterface>(new UrlFactory());
 expectAssignable<ServerBuilderInterface>(new ServerBuilder());
+
+const startup = new Startup(new Configuration());
+expectAssignable<StartupInterface>(startup);
+await startup.configure(new Container());
