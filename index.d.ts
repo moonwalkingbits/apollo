e6805809aa04678fae8315376d94758a6dcc782a
Martin Pettersson <martin_pettersson@outlook.com>
2021-02-24 21:47:53 +0100

    Remove doc comments from type declarations

--- index.d.ts
+++ index.d.ts
@@ -13,110 +13,21 @@ declare type ResponseStatus = import("@moonwalkingbits/apollo-http").ResponseSta
 declare type StartupInterface = import("@moonwalkingbits/apollo-server").StartupInterface;
 declare type StreamFactoryInterface = import("@moonwalkingbits/apollo-http").StreamFactoryInterface;
 
-/**
- * A minimal default startup routine.
- */
 declare class Startup implements StartupInterface {
-    /**
-     * Create a new startup instance.
-     *
-     * @param configuration Application configuration instance.
-     */
     public constructor(configuration: ConfigurationInterface);
-
-    /**
-     * Load and run all configured service providers.
-     *
-     * All service providers are constructed using the application container to
-     * enable dependency injection.
-     *
-     * @param container Application container instance.
-     * @return A promise resolving when the method is done.
-     */
     public configure(configuration: ContainerInterface): Promise<void>;
 }
 
-/**
- * Base class of all application HTTP controllers.
- *
- * This class provides a set of methods that are convenient when creating
- * HTTP responses.
- */
 declare abstract class AbstractController {
-    /**
-     * Response factory instance.
-     */
     protected responseFactory: ResponseFactoryInterface;
-
-    /**
-     * Stream factory instance.
-     */
     protected streamFactory: StreamFactoryInterface;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @param Response factory instance.
-     * @param streamFactory Stream factory instance.
-     */
     public constructor(responseFactory: ResponseFactoryInterface, streamFactory: StreamFactoryInterface);
-
-    /**
-     * Return an empty response.
-     *
-     * @return Empty response.
-     */
     protected noContent(): ResponseInterface;
-
-    /**
-     * Return a not found response.
-     *
-     * @return Not found response.
-     */
     protected notFound(): ResponseInterface;
-
-    /**
-     * Return an error response.
-     *
-     * @param message Error message.
-     * @param statusCode HTTP response status.
-     * @return Error response.
-     */
     protected error(message: string, statusCode?: ResponseStatus): ResponseInterface;
-
-    /**
-     * Return a text response.
-     *
-     * @param message Response message.
-     * @param statusCode HTTP response status.
-     * @return Text response.
-     */
     protected text(message: string, statusCode?: ResponseStatus): ResponseInterface;
-
-    /**
-     * Return a JSON response.
-     *
-     * @param content Response content.
-     * @param statusCode HTTP response status.
-     * @return JSON response.
-     */
     protected json(content: Object, statusCode?: ResponseStatus): ResponseInterface;
-
-    /**
-     * Return a created response.
-     *
-     * @param location Entity location.
-     * @return Created response.
-     */
     protected created(location: string): ResponseInterface;
-
-    /**
-     * Return a redirect response.
-     *
-     * @param location Redirect location.
-     * @param statusCode HTTP response status.
-     * @return Redirect response.
-     */
     protected redirect(location: string, statusCode?: ResponseStatus): ResponseInterface;
 }
 
--- routing.d.ts
+++ routing.d.ts
@@ -7,93 +7,16 @@
 
 declare type RequestMethod = import("@moonwalkingbits/apollo-http").RequestMethod;
 
-/**
- * Decorator defining a controller base route.
- *
- * @param pattern Base route pattern.
- * @return Class decorator handler.
- */
 declare function route(pattern: string): Function;
-
-/**
- * Decorator defining an HTTP action.
- *
- * @param method HTTP request method.
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function request(method: RequestMethod, pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP OPTIONS action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function OPTIONS(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP GET action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function GET(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP HEAD action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function HEAD(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP POST action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function POST(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP PUT action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function PUT(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP PATCH action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function PATCH(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP DELETE action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function DELETE(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP TRACE action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function TRACE(pattern?: string): Function;
-
-/**
- * Decorator defining an HTTP CONNECT action.
- *
- * @param pattern Action pattern.
- * @return Method decorator handler.
- */
 declare function CONNECT(pattern?: string): Function;
 
 export {
