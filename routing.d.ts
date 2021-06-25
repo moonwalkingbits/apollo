4339fac413dda32e5e4830a1068d358754e6f326
Martin Pettersson <martin.pettersson@nordicnest.com>
2021-06-25 06:06:35 +0200

    Export additional types from routing module

--- routing.d.ts
+++ routing.d.ts
@@ -5,7 +5,12 @@
  * file that was distributed with this source code.
  */
 
+declare type RequestInterface = import("@moonwalkingbits/apollo-http").RequestInterface;
 declare type RequestMethod = import("@moonwalkingbits/apollo-http").RequestMethod;
+declare type ResponseFactoryInterface = import("@moonwalkingbits/apollo-http").ResponseFactoryInterface;
+declare type ResponseInterface = import("@moonwalkingbits/apollo-http").ResponseInterface;
+
+declare const DECORATOR_METADATA_KEY: Symbol;
 
 declare function route(pattern: string): Function;
 declare function request(method: RequestMethod, pattern?: string): Function;
@@ -19,8 +24,27 @@ declare function DELETE(pattern?: string): Function;
 declare function TRACE(pattern?: string): Function;
 declare function CONNECT(pattern?: string): Function;
 
+declare class Route {
+    public method: RequestMethod;
+    public pattern: RegExp | string;
+    public action: Function;
+    public constructor(method: RequestMethod, pattern: RegExp | string, action: Function);
+}
+
+declare class Router {
+    public constructor(routes: Array<Route>, responseFactory: ResponseFactoryInterface);
+    public route(request: RequestInterface): ResponseInterface | Promise<ResponseInterface>;
+}
+
+declare class RouterBuilder {
+    public constructor(responseFactory: ResponseFactoryInterface);
+    public addRoute(method: RequestMethod, pattern: RegExp | string, action: Function): this;
+    public build(): Router;
+}
+
 export {
     CONNECT,
+    DECORATOR_METADATA_KEY,
     DELETE,
     GET,
     HEAD,
@@ -28,6 +52,9 @@ export {
     PATCH,
     POST,
     PUT,
+    Route,
+    Router,
+    RouterBuilder,
     TRACE,
     request,
     route
